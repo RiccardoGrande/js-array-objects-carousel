1f5436906481c6176dadd0be9b480d21c2c53fdc
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-23 15:57:46 +0100

    try add active structure for buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,4 +85,66 @@ for (let i = 0; i < images.length; i++) {
         continue;
 
     }
+
+
 }
+
+
+
+
+// selezioniamo tutte le slide
+const slideImagesElements = document.querySelectorAll('.col > .images > img')  
+    console.log(slideImagesElements);
+
+// creiamo la funzione per attivazione del click per next
+
+const nextEl = document.querySelector('.next')
+nextEl.addEventListener('click', function () {
+  console.log('cliccato next');
+
+   // selezioniamo la slide corrente
+   const currentSlide = slideImagesElements[activeImage]
+   console.log(currentSlide);
+
+   // rimuoviamo le classi active dalle immagini in active
+   currentSlide.classList.remove('active')
+
+   // aumentiamo il valore della variabile activeImage
+   activeImage++ 
+
+   // selezioniamo la slide successiva
+   console.log(activeImage);
+   const nextImage = slideImagesElements[activeImage]
+
+   // aggiungiamo la classe active
+   console.log(nextImage);
+   nextImage.classList.add('active')
+
+})
+
+
+// creiamo la funzione per attivazione del click per prev
+const prevEl = document.querySelector('.prev')
+prevEl.addEventListener('click', function () {
+  console.log('cliccato prev');
+
+  console.log(slideImagesElements); 
+
+  // selezioniamo la slide corrente
+  const currentSlide = slideImagesElements[activeImage]
+  console.log(currentSlide);
+
+  // rimuoviamo le classi active dalle immagini in active
+  currentSlide.classList.remove('active')
+
+  // diminuiamo il valore della variabile activeImage
+  activeImage-- 
+  // selezioniamo la prossima slide
+  console.log(activeImage);
+  const nextImage = slideImagesElements[activeImage]
+
+  // aggiungiamo la classe active
+  console.log(nextImage);
+  nextImage.classList.add('active')
+
+})
